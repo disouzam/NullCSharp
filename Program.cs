a072eaac08b30a3b2f28635808dfcecba8c7a7e6
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-07-09 21:54:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
