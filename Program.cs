93cbb3edb08d3798efe1a85e35a785616ea949f5
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-07-09 22:00:20 -0300

    Original sample code from https://dev.to/matheusgomes062/coalescing-operator-and-compound-assignment-operator-in-c-3ae3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿// Testing instructions from this article below
+// https://dev.to/matheusgomes062/coalescing-operator-and-compound-assignment-operator-in-c-3ae3
+
+using System;
 
 namespace NullCSharp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      double? x = null;
+      double? y = 10.0;
+
+      // check if x is null
+      if (x.HasValue)
+        Console.WriteLine(x.Value);
+      else
+        Console.WriteLine("X is null");
+
+      // check if y is null
+      if (y.HasValue)
+        Console.WriteLine(y.Value);
+      else
+        Console.WriteLine("Y is null");
     }
+  }
 }
